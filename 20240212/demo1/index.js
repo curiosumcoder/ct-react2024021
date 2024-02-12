f91b9fb15adfa1833dc06e3e7ad8aff9f23fee8c
Gilberto Bermúdez Garro <gbermude@outlook.com>
2024-02-12 12:01:35 -0600

    Update

---
+++ 20240212/demo1/index.js
@@ -0,0 +1,12 @@
+function helloWorld() {
+    console.log('Hello World');    
+}
+
+function helloWorld2() {
+    console.log('Hello World2');    
+}
+
+export default helloWorld;
+
+//export { helloWorld, helloWorld2 }
+export { helloWorld2 }
